2de4b9b48076e126a84d6997f7dca2280ac60725
kando-o <64841289+kando-o@users.noreply.github.com>
2025-06-26 04:49:42 +0200

    RS DEPANNAGE config New router oublie

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
-
+import App from '../App';
 const Hero = () => {
   const heroRef = useRef(null);
   const textRefs = useRef([]);
@@ -34,43 +34,48 @@ const Hero = () => {
   
 
   return (
-    <div ref={heroRef} className="bg-white text-center py-24 md:py-24 px-4">
-      <h1
-        ref={(el) => textRefs.current[0] = el}
-        className="text-4xl md:text-6xl lg:text-7xl font-bold mb-4 md:mb-6"
-      >
-        Enlèvement <span className="bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text"> d'épaves,</span>
-        <br />
-        <span className='text-3xl md:text-4xl lg:text-5xl'>Gratuit sur toute la France</span>
-      </h1>
-      <p
-        ref={(el) => textRefs.current[1] = el}
-        className="text-base md:text-lg lg:text-xl text-gray-600 max-w-3xl mx-auto mt-4 md:mt-6"
-      >
-        Vendre sa voiture avec <span className='font-bold'>RS DEPANNAGE</span>, c'est facile, rapide & sûr!
-      </p>
-      <p
-        ref={(el) => textRefs.current[2] = el}
-        className="text-base md:text-lg lg:text-xl text-gray-600 mb-6 md:mb-8 max-w-3xl mx-auto"
-      >
-        RD DEPANNAGE s'engage à prendre en charge tous types de véhicules: normal, étranger, véhicules gagés et/ou accidentés.
-      </p>
-      <div className="flex flex-col w-[50%] md:w-[50%] lg:w-[30%] mx-auto gap-2">
-        <button
-          ref={buttonRef}
-          onClick={handleClickEvent}
-          className="bg-gray-900 text-white font-semibold py-2 px-5 sm:py-3 sm:px-6 rounded-full text-sm sm:text-base md:text-lg hover:bg-gray-800 transition-colors"
-        >
-          En savoir plus ! →
-        </button>
-        <button
-          className="bg-gray-900 text-white font-semibold py-2 px-5 sm:py-3 sm:px-6 rounded-full text-sm sm:text-base md:text-lg"
-          disabled
-        >
-          06 51 60 58 83
-        </button>
-      </div>
-    </div>
+		<>
+			<App />
+			<div ref={heroRef} className="bg-white text-center py-24 md:py-24 px-4">
+				<h1
+					ref={(el) => textRefs.current[0] = el}
+					className="text-4xl md:text-6xl lg:text-7xl font-bold mb-4 md:mb-6"
+				>
+					 <span className="bg-gradient-to-r from-red-500 to-zinc-900 text-transparent bg-clip-text">Dépannage</span> & Remorquage 24/24 <span className="bg-gradient-to-r from-zinc-900 to-red-500 text-transparent bg-clip-text"> en Ile-De-France,</span>
+					<br />
+					<span className='text-3xl md:text-4xl lg:text-5xl'>Gratuit sur toute la France</span>
+				</h1>
+				<p
+					ref={(el) => textRefs.current[1] = el}
+					className="text-base md:text-lg lg:text-xl text-gray-600 max-w-3xl mx-auto mt-4 md:mt-6"
+				>
+					Dépannage et remorquage 24h/24 dans toute la France avec <span className='font-bold bg-gradient-to-r from-zinc-900 to-red-500 text-transparent bg-clip-text'>RS DEPANNAGE</span>.
+				</p>
+				<p
+					ref={(el) => textRefs.current[2] = el}
+					className="text-base md:text-lg lg:text-xl text-gray-600 mb-6 md:mb-8 max-w-3xl mx-auto"
+				>
+					Nous intervenons rapidement sur tous types de véhicules — en panne, accidentés, gagés ou non pour un remorquage fiable et un dépannage efficace. 
+					<br />
+					Et si vous souhaitez vendre votre voiture, nous vous accompagnons aussi dans une procédure simple, rapide et sécurisée.
+				</p>
+				<div className="flex flex-col w-[50%] md:w-[50%] lg:w-[30%] mx-auto gap-2">
+					<button
+						ref={buttonRef}
+						onClick={handleClickEvent}
+						className="bg-gray-900 text-white font-semibold py-2 px-5 sm:py-3 sm:px-6 rounded-full text-sm sm:text-base md:text-lg hover:bg-red-600 transition-colors"
+					>
+						En savoir plus ! →
+					</button>
+					<button
+						className="bg-gray-900 text-white font-semibold py-2 px-5 sm:py-3 sm:px-6 rounded-full text-sm sm:text-base md:text-lg"
+						disabled
+					>
+						06 51 60 58 83
+					</button>
+				</div>
+			</div>
+		</>
   );
 };
 
---
+++ src/page/home/views/home.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+function home() {
+
+	const [loading, setLoading] = useState(false);
+	const metaImage = "https://RSDEPANNAGE.fr/assets/pic2-B4MtFGv-.jpg";
+
+	if (loading) {
+		return <Loading />; 
+	}
+
+	return (
+			<HelmetProvider>
+				<Helmet>
+					<title>Enlèvement Gratuit et Rapide d'épaves de véhicules 91 - RS DEPANNAGE en Île-de-France et partout en France</title>
+					<meta
+						name="description"
+						content="Besoin d'un enlèvement d'épave rapide et gratuit dans le 91 ? RS DEPANNAGE intervient 7j/7 en Île-de-France et partout en France. Contactez-nous !" 
+					/>
+					<meta
+						name="keywords"
+						content="enlèvement épaves, enlèvement d'épave 91, camping car, enlèvement gratuit, enlèvement épaves gratuit, utilitaire, bateau, récupération véhicule, épaviste agréé, enlèvement épave Paris, vente voiture épave"
+					/>
+					<meta name="author" content="RS DEPANNAGE" />
+					<meta name="robots" content="index, follow" />
+					<link rel="canonical" href="https://RSDEPANNAGE.fr" /> {/* A changer */}
+
+					{/*  Balises Open Graph (Facebook, Whatsapp) */}
+
+					<meta property="og:site_name" content="RS DEPANNAGE" />
+					<meta property="og:locale" content="fr_FR" />
+					<meta property="og:title" content="Enlèvement Gratuit et Rapide d'Épaves 91 - RS DEPANNAGE Service rapide & gratuit en Île-de-France" />
+					<meta property="og:description" content="Service d'enlèvement gratuit et rapide des épaves en Île-de-France et partout en France. Contactez RS DEPANNAGE pour un service fiable et professionnel." />
+					<meta property="og:image" content={metaImage} />
+					<meta property="og:image:alt" content="RS DEPANNAGE - Enlèvement gratuit d'épaves en Île-de-France" />
+					<meta property="og:url" content="https://RS DEPANNAGE.fr" />
+					<meta property="og:type" content="website" />
+
+					{/* Balises Twitter Cards */}
+
+					<meta name="twitter:card" content="summary_large_image" />
+					<meta name="twitter:title" content="RS DEPANNAGE - Service d'Enlèvement des Épaves Gratuit" />
+					<meta name="twitter:description" content="Enlèvement gratuit des épaves en France. Fiabilité, rapidité et professionnalisme avec RS DEPANNAGE." />
+					<meta name="twitter:image" content={metaImage} />
+					<meta name="twitter:image:alt" content="Service rapide et gratuit pour l'enlèvement des épaves" />
+
+					<meta name="geo.region" content="FR" />
+					<meta name="geo.placename" content="Île-de-France" />
+					<meta name="geo.position" content="48.8566;2.3522" />				
+
+					<script type="application/ld+json">
+						{`
+							{
+							"@context": "https://schema.org",
+							"@type": "AutoService",
+							"name": "RS DEPANNAGE - Enlèvement des épaves 100% Gratuit en France",
+							"description": "RS DEPANNAGE offre un service d'enlèvement gratuit des épaves partout en France. Vendez votre véhicule avec facilité, rapidité et sécurité.",
+							"image": "https://RS DEPANNAGE.fr/assets/pic2-B4MtFGv-.jpg",
+							"url": "https://RS DEPANNAGE.fr",
+							"telephone": "+33622222018",
+							"address": {
+								"@type": "PostalAddress",
+								"streetAddress": "18 allée des Irlandais, 91300 Massy",
+								"addressLocality": "Louvres",
+								"addressRegion": "Île-de-France",
+								"postalCode": "91300",
+								"addressCountry": "France"
+							},
+							"openingHoursSpecification": [
+								{
+								"@type": "OpeningHoursSpecification",
+								"dayOfWeek": [
+									"Monday",
+									"Tuesday",
+									"Wednesday",
+									"Thursday",
+									"Friday",
+									"Saturday"
+								],
+								"opens": "08:00",
+								"closes": "18:00"
+								}
+							],
+							"geo": {
+								"@type": "GeoCoordinates",
+								"latitude": "49.0139",
+								"longitude": "2.4597"
+							},
+							"serviceType": "Dépannage & Remorquage 24/24, Achat de véhicules d'occasion",
+							"areaServed": "France",
+							"priceRange": "Gratuit"
+							}
+						`}
+					</script>
+				</Helmet>
+			</HelmetProvider>
+		);
+}
+
+export default home
